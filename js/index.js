edd2cc9dac90d5fbe31f0c4c73d2940bd5ecdbb1
gaozhiqiang <gaozq@zoteri.net>
2016-11-18 17:13:24 +0800

    change
    change

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
-var socket = io("https://127.0.0.1:8888");
+var socket = io("http://192.168.1.139:8888");
         socket.on('connected', function (data) {
-            socket.emit('server',{model:"user",action:"regest",data:{userName:"mouren",phone:"18520408028",key:"123456"}});
+            socket.emit('server',{model:"user",action:"regest",data:{userName:"moureAn",phone:"18520408028",key:"123456"}});
         });
         socket.on('regest',function(data){
-            console.log(data)
+            alert(JSON.stringify(data))
+        });
+        socket.on('login',function(data){
+            alert(data)
         });
         socket.on('err',function(data){
-            console.log(data)
+            alert(data)
         })
