f524e02b3341568dd7cf21bded58f8c5be5fbb53
gaozhiqiang <gaozq@zoteri.net>
2016-11-21 18:06:03 +0800

    change

--- js/index.js
+++ js/index.js
@@ -1,13 +1,28 @@
 var socket = io("http://192.168.1.139:8888");
         socket.on('connected', function (data) {
-            socket.emit('server',{model:"user",action:"regest",data:{userName:"moureAn",phone:"18520408028",key:"123456"}});
+            console.log(data);
+            if(localStorage.getItem('tk')){
+                socket.emit('tk',{tk:localStorage.getItem('tk')});
+            }
+            //socket.emit('server',{model:"user",action:"login",data:{userName:"moureAn",phone:"18520408028",key:"123456"}});
+        });
+        socket.on('tk',function(data){
+            if(data.tk){
+                localStorage.setItem('tk',data.tk);
+                socket.emit('server',{model:"user",action:"recharge",data:{money:1}});  
+            }else{
+                localStorage.removeItem('tk');
+            }
         });
         socket.on('regest',function(data){
-            alert(JSON.stringify(data))
+            console.log(data)
         });
         socket.on('login',function(data){
-            alert(data)
+            console.log(data);
+        });
+        socket.on('balance',function(data){
+            console.log(data)
         });
         socket.on('err',function(data){
-            alert(data)
+            console.log(data)
         })
